fe98d0927c91b82d46aa96f5226236a15b7ddea9
wiseguy56 <wiseguy56@hotmail.com>
2024-08-09 16:27:33 -0400

    Update fetchSvg.ts
     wrapped the getter in a try catch to let the rest work
    
    Signed-off-by: wiseguy56 <wiseguy56@hotmail.com>

--- src/wiring/fetchSvg.ts
+++ src/wiring/fetchSvg.ts
@@ -8,6 +8,7 @@ export default async function fetchSvg(
   wiringBookCode: string,
   languageCode: string
 ): Promise<string> {
+try {  
   const req = await client({
     method: "GET",
     url: `https://www.fordservicecontent.dealerconnection.com/ford_content/PublicationRuntimeRefreshPTS/wiring/svg/${environment}/${vehicleId}/~W${wiringBookCode}/${languageCode}/svg/${docNumber}/0/${parseInt(
@@ -17,5 +18,7 @@ export default async function fetchSvg(
       fromPageBase: "https://www.fordtechservice.dealerconnection.com",
     },
   });
+
   return req.data;
+} catch (error: any) { return ""; }
 }
